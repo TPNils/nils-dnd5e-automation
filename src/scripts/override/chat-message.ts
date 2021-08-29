52387d2104fe990d7f97d37046f3db506328f978
Nils <nils.yseboodt@gmail.com>
2021-08-29 05:27:32 +0200

    add comments

--- src/scripts/override/chat-message.ts
+++ src/scripts/override/chat-message.ts
@@ -1,6 +1,13 @@
 import { staticValues } from '../static-values';
 
 async function getHTML(this: ChatMessage, wrapped: (...args: any) => any, ...args: any[]): Promise<JQuery> {
+  // Add client side rendering of the template, specific for the user.
+  // Pro:
+  // + This allows templates to be rendered specifically for the user (ex: based on permissions)
+  // + and should also reduce network traffic (not sure how impactfull it actually is)
+  // Con: 
+  // - Extra CPU power required by the client
+  // - Templates won't be shown properly once the module is uninstalled => would be an issue anyway, this might even be cleaner
   const clientTemplate = this.getFlag(staticValues.moduleName, 'clientTemplate') as string;
   const clientTemplateData = this.getFlag(staticValues.moduleName, 'clientTemplateData') as any;
   if (clientTemplate && clientTemplateData) {
