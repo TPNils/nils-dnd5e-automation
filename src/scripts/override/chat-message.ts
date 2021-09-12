8d5b15d945b3bbf6f90cc861d678ceb2a9ff7678
Nils <nils.yseboodt@gmail.com>
2021-09-12 22:59:10 +0200

    implement bug reporter

--- src/scripts/override/chat-message.ts
+++ src/scripts/override/chat-message.ts
@@ -15,7 +15,52 @@ async function getHTML(this: ChatMessage, wrapped: (...args: any) => any, ...arg
     if (game.user.isGM) {
       options.secrets = true;
     }
-    this.data.update({content: TextEditor.enrichHTML(await renderTemplate(clientTemplate, clientTemplateData), options as any)})
+    try {
+      this.data.update({content: TextEditor.enrichHTML(await renderTemplate(clientTemplate, clientTemplateData), options as any)})
+    } catch (e) {
+      console.error(e);
+
+      let errorString: string;
+      if (e instanceof Error) {
+        errorString = `${e.name}: ${e.message}\n\n${e.stack}`;
+      } else {
+        errorString = String(e);
+      }
+      if (game.modules.get('bug-reporter')?.active) {
+        const message = document.createElement('div');
+        message.append(document.createTextNode("Internal error rendering the message."));
+        const report = document.createElement('button');
+        report.innerText = 'Please report the bug';
+        report.setAttribute('onclick', `game.modules.get("bug-reporter").api.bugWorkflow("nils-automated-compendium", "Error rendering a message", "${errorString.replace(/"/g, '\\"').replace(/\n/g, '\\n')}")`)
+        message.append(report);
+
+        const wrapper = document.createElement('div');
+        wrapper.append(message);
+        this.data.update({content: wrapper.innerHTML});
+      } else {
+        const bugsUrl = game.modules.get(staticValues.moduleName).data.bugs;
+        const message = document.createElement('div');
+        message.append(document.createTextNode("Internal error rendering the message. Please "));
+        const aUrl = document.createElement('a');
+        aUrl.innerText = 'report the bug';
+        aUrl.setAttribute('href', bugsUrl);
+        message.append(aUrl);
+        message.append(document.createTextNode(" with the follwoing message"));
+
+        const errorNode = document.createElement('code');
+        errorNode.style.maxHeight = '200px';
+        errorNode.style.display = 'block';
+        errorNode.style.overflowY = 'auto';
+        errorNode.style.userSelect = 'text';
+        errorNode.innerText = errorString;
+
+        const wrapper = document.createElement('div');
+        wrapper.append(message);
+        wrapper.append(document.createElement('br'));
+        wrapper.append(errorNode);
+        this.data.update({content: wrapper.innerHTML});
+      }
+    }
   }
 
   return wrapped(args);
