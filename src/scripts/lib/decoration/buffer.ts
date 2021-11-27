4272213d6453fc770341eb7a1ac9b5901d56362d
Nils <nils.yseboodt@gmail.com>
2021-11-27 16:40:19 +0100

    fix: retain this context correctly

--- src/scripts/lib/decoration/buffer.ts
+++ src/scripts/lib/decoration/buffer.ts
@@ -1,3 +1,20 @@
+function bufferInternal<T>(bufferTime: number, originalFunction: (args: Array<Array<T>>) => any): (...args: T[]) => any {
+  let bufferedCalls: Array<Array<T>> = [];
+  // use 'function' to retain the original context from the caller (use another this context)
+  return function (...args: T[]): any {
+    const isFirstCall = bufferedCalls.length === 0;
+    bufferedCalls.push(args);
+    if (isFirstCall) {
+      // use '=>' to keep the context within this function (don't create a new this context)
+      setTimeout(() => {
+        const calls = bufferedCalls;
+        bufferedCalls = [];
+        originalFunction.call(this, calls);
+      }, bufferTime);
+    }
+  }
+}
+
 export function buffer(args: {bufferTime?: number} = {}) {
   const bufferTime = args.bufferTime ?? 0;
 
@@ -12,34 +29,14 @@ export function buffer(args: {bufferTime?: number} = {}) {
       throw new Error(`Can't change the property ${propertyKey}. property is not writable.`);
     }
     
-    const bufferInternal = new BufferInternal(
+    const bufferMethod = bufferInternal(
       bufferTime,
       descriptor.get ? descriptor.get() : descriptor.value
     )
     if (descriptor.get) {
-      descriptor.set(bufferInternal.bufferMethod);
+      descriptor.set(bufferMethod);
     } else {
-      descriptor.value = bufferInternal.bufferMethod;
+      descriptor.value = bufferMethod;
     }
   };
-}
-
-class BufferInternal<T> {
-  constructor(
-    private readonly bufferTime: number,
-    private readonly originalFunction: (args: Array<Array<T>>) => any,
-  ){}
-
-  private bufferedCalls: Array<Array<T>> = [];
-  public bufferMethod = (...args: T[]): any => {
-    const isFirstCall = this.bufferedCalls.length === 0;
-    this.bufferedCalls.push(args);
-    if (isFirstCall) {
-      setTimeout(() => {
-        const bufferedCalls = this.bufferedCalls;
-        this.bufferedCalls = [];
-        this.originalFunction(bufferedCalls);
-      }, this.bufferTime);
-    }
-  }
 }
