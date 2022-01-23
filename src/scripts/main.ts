0ff63231ce75fda9d980e45dccc8065367e8e642
Nils <nils.yseboodt@gmail.com>
2022-01-23 17:25:25 +0100

    Disable UtilsChatMessage

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,7 +2,6 @@ import { registerHooks as registerGlobalApiHooks } from "./global-api";
 import { registerHooks as registerHtmlHooks } from "./global-html-listener";
 import { registerHooks as registerOverrideHooks } from "./override/index.js";
 import { provider } from "./provider/provider";
-import { UtilsChatMessage } from "./utils/utils-chat-message";
 import { UtilsHandlebars } from "./utils/utils-handlebars";
 import { ModularCard } from "./modular-card/modular-card";
 import { AttackCardPart } from "./modular-card/attack-card-part";
@@ -11,7 +10,6 @@ import { DamageCardPart } from "./modular-card/damage-card-part";
 registerGlobalApiHooks();
 registerHtmlHooks();
 provider.registerHooks();
-UtilsChatMessage.registerHooks();
 registerOverrideHooks()
 UtilsHandlebars.registerHooks();
 ModularCard.registerHooks();
