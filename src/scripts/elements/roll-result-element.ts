568d8c8cffce094ca1ba994faca4dfce03e1320e
Nils <nils.yseboodt@gmail.com>
2022-04-02 02:13:46 +0200

    Convert roll result element to ElementBuilder

--- src/scripts/elements/roll-result-element.ts
+++ src/scripts/elements/roll-result-element.ts
@@ -1,11 +1,12 @@
-import { buffer } from "../lib/decorator/buffer";
 import { RunOnce } from "../lib/decorator/run-once";
 import { RollData, UtilsRoll } from "../lib/roll/utils-roll";
-import { MemoryStorageService, MemoryValue } from "../service/memory-storage-service";
+import { MemoryStorageService } from "../service/memory-storage-service";
 import { staticValues } from "../static-values";
-import { UtilsElement } from "./utils-element";
+import { ElementBuilder, ElementCallbackBuilder } from "./element-builder";
 
-export class RollResultElement extends HTMLElement {
+const elementsBySlotNameSymbol = Symbol('elementsBySlotName');
+
+export class RollResultElement {
 
   public static selector(): string {
     return `${staticValues.code}-roll-result`;
@@ -13,70 +14,68 @@ export class RollResultElement extends HTMLElement {
 
   @RunOnce()
   public static registerHooks(): void {
-    customElements.define(RollResultElement.selector(), RollResultElement);
-  }
-  
-  public static get observedAttributes() {
-    return [
-      // Required
-      'data-roll',
-      // Optional
-      'data-compact',
-      'data-highlight-total-on-firstTerm',
-      'data-override-formula',
-      'data-override-max-roll',
-    ];
+    new ElementBuilder()
+      .listenForAttribute('data-roll', 'json')
+      .listenForAttribute('data-highlight-total-on-firstTerm', 'boolean')
+      .listenForAttribute('data-override-formula', 'string')
+      .listenForAttribute('data-override-max-roll', 'number')
+      .addOnInit(RollResultElement.extractInputSlots)
+      .addOnInit((context) => {
+        context.addStoppable(MemoryStorageService.getElementValue(context.element, 'roll-open').listen(value => RollResultElement.setOpenState(context.element, !!value)));
+      })
+      .addOnAttributeChange(async ({element, attributes}) => {
+        const rollJson: RollData = attributes['data-roll'];
+        if (!rollJson?.evaluated) {
+          element.textContent = '';
+          return;
+        }
+        const html = await renderTemplate(
+          `modules/${staticValues.moduleName}/templates/roll/roll.hbs`, {
+            roll: UtilsRoll.fromRollData(rollJson),
+            overrideFormula: attributes['data-override-formula'],
+            highlightTotalOnFirstTerm: attributes['data-highlight-total-on-firstTerm'],
+            overrideMaxRoll: attributes['data-override-max-roll'],
+          }
+        );
+
+        const root = document.createElement('div');
+        root.innerHTML = html;
+        RollResultElement.injectSlots({element, detatchedRoot: root});
+        
+        const fragment = document.createDocumentFragment();
+        fragment.append(...Array.from(root.childNodes));
+        element.textContent = '';
+        element.append(fragment);
+        RollResultElement.setOpenState(element, !!MemoryStorageService.getElementValue(element, 'roll-open').get());
+      })
+      .addListener(new ElementCallbackBuilder()
+        .setEvent('click')
+        .setExecute(({element}) => {
+          const value = MemoryStorageService.getElementValue(element, 'roll-open');
+          value.set(!value.get());
+        }))
+      .build(RollResultElement.selector())
   }
 
-  private unregister: ReturnType<MemoryValue['listen']>;
-  private elementsBySlotName: Map<string, Element[]> = new Map();
-  public connectedCallback(): void {
-    this.addEventListener('click', () => this.toggleOpen());
-    this.unregister = MemoryStorageService.getElementValue(this, 'roll-open').listen(value => this.setOpenState(!!value));
-
+  private static extractInputSlots({element}: {element: HTMLElement}): void {
     const elementsBySlotName = new Map<string, Element[]>();
-    this.querySelectorAll('[slot]').forEach(element => {
+    element.querySelectorAll('[slot]').forEach(element => {
       if (!elementsBySlotName.has(element.getAttribute('slot'))) {
         elementsBySlotName.set(element.getAttribute('slot'), []);
       }
       elementsBySlotName.get(element.getAttribute('slot')).push(element);
     });
-    this.elementsBySlotName = elementsBySlotName;
-    this.textContent = '';
-    this.calcInner();
+    element[elementsBySlotNameSymbol] = elementsBySlotName;
   }
 
-  @buffer()
-  public attributeChangedCallback(args: Array<[string/* attributeName */, string/* oldValue */, string/* newValue */]>): void {
-    this.calcInner();
-  }
-
-  public disconnectedCallback(): void {
-    if (this.unregister) {
-      this.unregister.unregister();
+  private static injectSlots({element, detatchedRoot}: {element: HTMLElement, detatchedRoot?: HTMLElement}): void {
+    const elementsBySlotName: Map<string, Element[]> = element[elementsBySlotNameSymbol];
+    if (!elementsBySlotName) {
+      return
     }
-  }
-
-  private async calcInner(): Promise<void> {
-    const rollJson: RollData = UtilsElement.readAttrJson(this, 'data-roll');
-    if (!rollJson?.evaluated) {
-      this.textContent = '';
-      return;
-    }
-    const html = await renderTemplate(
-      `modules/${staticValues.moduleName}/templates/roll/roll.hbs`, {
-        roll: UtilsRoll.fromRollData(rollJson),
-        overrideFormula: UtilsElement.readAttrString(this, 'data-override-formula'),
-        highlightTotalOnFirstTerm: UtilsElement.readAttrBoolean(this, 'data-highlight-total-on-firstTerm'),
-        overrideMaxRoll: UtilsElement.readAttrInteger(this, 'data-override-max-roll'),
-      }
-    );
-
-    const root = document.createElement('div');
-    root.innerHTML = html;
-    for (const slotName of this.elementsBySlotName.keys()) {
-      const replaceElements = this.elementsBySlotName.get(slotName);
-      const slots = root.querySelectorAll(`slot[name="${slotName}"]`);
+    for (const slotName of elementsBySlotName.keys()) {
+      const replaceElements = elementsBySlotName.get(slotName);
+      const slots = (detatchedRoot == null ? element : detatchedRoot).querySelectorAll(`slot[name="${slotName}"]`);
       slots.forEach(slot => {
         for (let i = replaceElements.length - 1; i >= 0; i--) {
           slot.parentNode.insertBefore(replaceElements[i].cloneNode(true), slot);
@@ -86,18 +85,10 @@ export class RollResultElement extends HTMLElement {
         slot.parentNode.removeChild(slot);
       });
     }
-    this.textContent = '';
-    this.append(...Array.from(root.childNodes));
-    this.setOpenState(!!MemoryStorageService.getElementValue(this, 'roll-open').get());
   }
 
-  private toggleOpen(): void {
-    const value = MemoryStorageService.getElementValue(this, 'roll-open');
-    value.set(!value.get());
-  }
-
-  private setOpenState(shouldBeOpen: boolean): void {
-    const wrapper = this.querySelector(':scope > .wrapper');
+  private static setOpenState(element: HTMLElement, shouldBeOpen: boolean): void {
+    const wrapper = element.querySelector(':scope > .wrapper');
     if (!wrapper) {
       return;
     }
