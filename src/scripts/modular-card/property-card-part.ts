dff32a06ee4fea96b9322168852efe5ddbd89c89
Nils <nils.yseboodt@gmail.com>
2022-04-04 22:40:29 +0200

    migrate property card

--- src/scripts/modular-card/property-card-part.ts
+++ src/scripts/modular-card/property-card-part.ts
@@ -1,7 +1,9 @@
+import { ElementBuilder } from "../elements/element-builder";
 import { RunOnce } from "../lib/decorator/run-once";
 import { staticValues } from "../static-values";
 import { MyItem } from "../types/fixed-types";
 import { createElement, HtmlContext } from "./card-part-element";
+import { ItemCardHelpers } from "./item-card-helpers";
 import { ModularCard } from "./modular-card";
 import { ModularCardCreateArgs, ModularCardPart } from "./modular-card-part";
 
@@ -30,10 +32,20 @@ export class PropertyCardPart implements ModularCardPart<PropertyCardData> {
 
   @RunOnce()
   public registerHooks(): void {
-    createElement({
-      selector: this.getSelector(),
-      getHtml: context => this.getElementHtml(context),
-    });
+    new ElementBuilder()
+      .listenForAttribute('data-part-id', 'string')
+      .listenForAttribute('data-message-id', 'string')
+      .addOnAttributeChange(({element, attributes}) => {
+        return ItemCardHelpers.ifAttrData({attr: attributes, element, type: this, callback: async ({part}) => {
+          element.innerHTML = await renderTemplate(
+            `modules/${staticValues.moduleName}/templates/modular-card/property-part.hbs`, {
+              data: part.data,
+              moduleName: staticValues.moduleName
+            });
+          
+        }});
+      })
+      .build(this.getSelector())
     
     ModularCard.registerModularCardPart(staticValues.moduleName, this);
   }
@@ -49,16 +61,6 @@ export class PropertyCardPart implements ModularCardPart<PropertyCardData> {
 
   public getHtml(data: HtmlContext): string {
     return `<${this.getSelector()} data-part-id="${data.partId}" data-message-id="${data.messageId}"></${this.getSelector()}>`
-  }
-
-  public getElementHtml(context: HtmlContext<PropertyCardData>): string | Promise<string> {
-    return renderTemplate(
-      `modules/${staticValues.moduleName}/templates/modular-card/property-part.hbs`, {
-        data: context.data,
-        messageId: context.messageId,
-        moduleName: staticValues.moduleName
-      }
-    );
   }
   //#endregion
 
