7bb14e89d89293f81fc33d01965bd0dd778eb96c
Nils <nils.yseboodt@gmail.com>
2022-06-02 16:17:56 +0200

    Add label slot for d20 roll

--- src/scripts/elements/roll-d20-element.ts
+++ src/scripts/elements/roll-d20-element.ts
@@ -10,6 +10,8 @@ export interface RollD20Data {
   overrideMaxRoll?: number;
 }
 
+const elementsBySlotNameSymbol = Symbol('elementsBySlotName');
+
 export class RollD20Element {
 
   public static selector(): string {
@@ -30,6 +32,7 @@ export class RollD20Element {
       .listenForAttribute('data-label', 'string')
       .listenForAttribute('data-override-formula', 'string')
       .listenForAttribute('data-override-max-roll', 'number')
+      .addOnInit(RollD20Element.extractInputSlots)
       .build(RollD20Element.selector())
   }
 
@@ -46,7 +49,8 @@ export class RollD20Element {
     } else if (firstTerm.modifiers?.includes('kl')) {
       mode = 'disadvantage';
     }
-    element.innerHTML = await renderTemplate(
+    const root = document.createElement('div');
+    root.innerHTML = await renderTemplate(
       `modules/${staticValues.moduleName}/templates/roll/roll-d20.hbs`, {
         roll: rollJson,
         mode: mode,
@@ -62,6 +66,43 @@ export class RollD20Element {
         moduleName: staticValues.moduleName,
       }
     );
+    RollD20Element.injectSlots({element, detatchedRoot: root});
+    
+    const fragment = document.createDocumentFragment();
+    fragment.append(...Array.from(root.childNodes));
+    element.textContent = '';
+    element.append(fragment);
+  }
+
+  // TODO either don't support slots or solve it in element builder => can't find a simple solution there
+  private static extractInputSlots({element}: {element: HTMLElement}): void {
+    const elementsBySlotName = new Map<string, Element[]>();
+    element.querySelectorAll('[slot]').forEach(element => {
+      if (!elementsBySlotName.has(element.getAttribute('slot'))) {
+        elementsBySlotName.set(element.getAttribute('slot'), []);
+      }
+      elementsBySlotName.get(element.getAttribute('slot')).push(element);
+    });
+    element[elementsBySlotNameSymbol] = elementsBySlotName;
+  }
+
+  private static injectSlots({element, detatchedRoot}: {element: HTMLElement, detatchedRoot?: HTMLElement}): void {
+    const elementsBySlotName: Map<string, Element[]> = element[elementsBySlotNameSymbol];
+    if (!elementsBySlotName) {
+      return
+    }
+    for (const slotName of elementsBySlotName.keys()) {
+      const replaceElements = elementsBySlotName.get(slotName);
+      const slots = (detatchedRoot == null ? element : detatchedRoot).querySelectorAll(`slot[name="${slotName}"]`);
+      slots.forEach(slot => {
+        for (let i = replaceElements.length - 1; i >= 0; i--) {
+          slot.parentNode.insertBefore(replaceElements[i].cloneNode(true), slot);
+        }
+      });
+      slots.forEach(slot => {
+        slot.parentNode.removeChild(slot);
+      });
+    }
   }
 
 }
