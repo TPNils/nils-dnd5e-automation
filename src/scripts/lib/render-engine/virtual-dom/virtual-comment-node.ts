f53f9f0ef3b96c6bc2cf0ab936de7901bf2bb854
Nils <nils.yseboodt@gmail.com>
2022-10-07 21:49:10 +0200

    Add comment node

---
+++ src/scripts/lib/render-engine/virtual-dom/virtual-comment-node.ts
@@ -0,0 +1,66 @@
+import { VirtualAttributeNode, VirtualChildNode, VirtualEventNode, VirtualNode, VirtualParentNode } from "./virtual-node";
+
+export class VirtualCommmentNode extends VirtualChildNode() implements VirtualNode {
+
+  public constructor(nodeValue?: string) {
+    super();
+    this.nodeValue = nodeValue;
+  }
+
+  get nodeName(): string {
+    return '#comment';
+  }
+  #nodeValue: string = '';
+  public get nodeValue(): string {
+    return this.#nodeValue;
+  }
+  public set nodeValue(value: string) {
+    if (value == null) {
+      this.#nodeValue = '';
+    } else {
+      this.#nodeValue = String(value);
+    }
+  }
+
+  public cloneNode(deep?: boolean): this {
+    const clone = new VirtualCommmentNode();
+    clone.#nodeValue = this.#nodeValue;
+    clone.startChildClone(this, deep);
+    return clone as this;
+  }
+
+  #node: Comment;
+  #appliedState: this;
+  public domNode(): Node {
+    if (this.#node == null) {
+      this.#node = new Comment(this.nodeValue);
+      this.#appliedState = this.cloneNode(false);
+    }
+    return this.#node;
+  }
+
+  public executeUpdate(): void {
+    if (this.#appliedState.nodeValue !== this.#nodeValue) {
+      this.#node.nodeValue = this.#nodeValue;
+      this.#appliedState = this.cloneNode(false);
+    }
+  }
+
+  public isNode(): this is VirtualNode {
+    return true;
+  }
+
+  public isAttributeNode(): this is VirtualAttributeNode {
+    return false;
+  }
+
+  public isEventNode(): this is VirtualEventNode {
+    return false;
+  }
+
+  public isParentNode(): this is VirtualParentNode {
+    return false;
+  }
+
+  
+}
