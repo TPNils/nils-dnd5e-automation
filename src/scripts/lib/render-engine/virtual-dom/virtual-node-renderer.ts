129597aa3979e0c43fa3d3b5e0d209fb4527d05d
Nils <nils.yseboodt@gmail.com>
2022-10-09 17:16:02 +0200

    support updating already created nodes

--- src/scripts/lib/render-engine/virtual-dom/virtual-node-renderer.ts
+++ src/scripts/lib/render-engine/virtual-dom/virtual-node-renderer.ts
@@ -1,5 +1,6 @@
 import { AttributeParser } from "../attribute-parser";
-import { VirtualChildNode, VirtualNode, VirtualParentNode } from "./virtual-node";
+import { StoredEventCallback, VirtualAttributeNode, VirtualChildNode, VirtualEventNode, VirtualNode, VirtualParentNode } from "./virtual-node";
+import { VirtualTextNode } from "./virtual-text-node";
 
 const stateSymbol = Symbol('domCache');
 export interface RenderState<T extends VirtualNode = VirtualNode> {
@@ -17,7 +18,12 @@ export class VirtualNodeRenderer {
     virtualNode[stateSymbol] = domNode;
   }
   
-  public static renderDom<T extends VirtualNode>(virtualNode: T): ReturnType<T['createDom']> {
+  /**
+   * @param virtualNode The virtual node that needs to be converted to a DOM element
+   * @param deepUpdate when false and the node already exists, only update the node itself, not it's children
+   * @returns Created or updated DOM element
+   */
+  public static renderDom<T extends VirtualNode>(virtualNode: T, deepUpdate: boolean = false): ReturnType<T['createDom']> {
     let pending: Array<{parent?: VirtualParentNode, node: VirtualNode}> = [{
       node: virtualNode,
     }];
@@ -26,8 +32,7 @@ export class VirtualNodeRenderer {
       pending = [];
       for (const process of processing) {
         const state = VirtualNodeRenderer.getOrNewState(process.node);
-        // TODO if (state.lastRenderSelfState == null) {
-        if (true) {
+        if (state.lastRenderSelfState == null) {
           // First time render
           if (process.node.isAttributeNode()) {
             for (const attr of process.node.getAttributeNames()) {
@@ -55,7 +60,63 @@ export class VirtualNodeRenderer {
 
           state.lastRenderSelfState = process.node.cloneNode(false);
         } else {
-          // TODO Update
+          let stateChanged = false;
+          if (process.node.isAttributeNode()) {
+            for (const attr of process.node.getAttributeNames()) {
+              const value = process.node.getAttribute(attr);
+              if ((state.lastRenderSelfState as VirtualAttributeNode & VirtualNode).getAttribute(attr) !== value) {
+                (state.domNode as Element).setAttribute(attr, value);
+                stateChanged = true;
+              }
+            }
+            
+            for (const attr of (state.lastRenderSelfState as VirtualAttributeNode & VirtualNode).getAttributeNames()) {
+              if (!process.node.hasAttribute(attr)) {
+                (state.domNode as Element).removeAttribute(attr);
+                stateChanged = true;
+              }
+            }
+          }
+
+          if (process.node.isEventNode()) {
+            const oldListeners = new Map<number, StoredEventCallback>();
+            for (const listener of (state.lastRenderSelfState as VirtualEventNode & VirtualNode).getEventListerners()) {
+              oldListeners.set(listener.guid, listener);
+            }
+            
+            for (const listener of process.node.getEventListerners()) {
+              if (oldListeners.has(listener.guid)) {
+                oldListeners.delete(listener.guid);
+              } else {
+                state.domNode.addEventListener(listener.type, listener.callback, listener.options);
+                stateChanged = true;
+              }
+            }
+
+            for (const listener of oldListeners.values()) {
+              state.domNode.removeEventListener(listener.type, listener.callback, listener.options);
+              stateChanged = true;
+            }
+          }
+          
+          if (process.node.isTextNode()) {
+            if (process.node.getText() !== (state.lastRenderSelfState as VirtualTextNode & VirtualNode).getText()) {
+              state.domNode.nodeValue = process.node.getText();
+              stateChanged = true;
+            }
+          }
+
+          if (stateChanged) {
+            state.lastRenderSelfState = process.node.cloneNode(false);
+          }
+          
+          // TODO new/deleted children
+          
+          if (deepUpdate && process.node.isParentNode()) {
+            for (const child of process.node.childNodes) {
+              pending.push({parent: process.node, node: child});
+            }
+          }
         }
       }
     }
@@ -71,8 +132,6 @@ export class VirtualNodeRenderer {
       };
       VirtualNodeRenderer.setState(virtualNode, state);
     }
-
-
     return state;
   }
 
