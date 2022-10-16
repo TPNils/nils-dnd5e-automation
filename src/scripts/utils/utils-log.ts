fe30d5cb1e99c2b88def38b426b82181be148986
Nils <nils.yseboodt@gmail.com>
2022-10-16 14:17:37 +0200

    fix: UtilsLog actually shows the call origin

--- src/scripts/utils/utils-log.ts
+++ src/scripts/utils/utils-log.ts
@@ -2,20 +2,18 @@ import { staticValues } from "../static-values";
 
 export class UtilsLog {
 
-  public static info(...args: any[]): void {
-    console.info(`%c${staticValues.moduleName}`, `color: #ff8f00`, ...args);
-  }
-  public static debug(...args: any[]): void {
-    console.debug(`%c${staticValues.moduleName}`, `color: #ff8f00`, ...args);
-  }
-  public static log(...args: any[]): void {
-    console.log(`%c${staticValues.moduleName}`, `color: #ff8f00`, ...args);
-  }
-  public static warn(...args: any[]): void {
-    console.warn(`%c${staticValues.moduleName}`, `color: #ff8f00`, ...args);
-  }
-  public static error(...args: any[]): void {
-    console.error(`%c${staticValues.moduleName}`, `color: #ff8f00`, ...args);
+  public static info = UtilsLog.createWithPrefix('info');
+  public static debug = UtilsLog.createWithPrefix('debug');
+  public static log = UtilsLog.createWithPrefix('log');
+  public static warn = UtilsLog.createWithPrefix('warn');
+  public static error = UtilsLog.createWithPrefix('error');
+
+  private static createWithPrefix<T extends keyof Console>(key: T): Console[T] {
+    if (typeof console[key] === 'function') {
+      return console[key].bind(console, `%c${staticValues.moduleName}`, `color: #ff8f00`);
+    } else {
+      return console[key];
+    }
   }
 
 }
