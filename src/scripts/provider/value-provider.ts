1276e5de302fa547eda7cddf0bf7f52e644bc659
Nils <nils.yseboodt@gmail.com>
2022-10-16 23:46:41 +0200

    expand on ValueProvider

--- src/scripts/provider/value-provider.ts
+++ src/scripts/provider/value-provider.ts
@@ -1,67 +1,35 @@
+import { RefValueWrite } from "../lib/ref-value/ref-value-write";
 import { Stoppable } from "../lib/utils/stoppable";
 
 /**
  * Allow to request a value before it may have been initialized, queueing the requests if the value is missing
+ * // TODO @deprecated
+ * @deprecated
  */
  export class ValueProvider<T = any> {
-  private nextListenerId = 0;
-  private listeners = new Map<number, (value?: T) => void>();
-  private valueProvided = false;
-  private value: T;
-  private requestFirstQueue: Array<(value?: T) => void> = [];
+  private delegate: RefValueWrite<T>;
 
   constructor(value?: T) {
-    this.value = value;
-    this.valueProvided = value !== undefined;
+    this.delegate = new RefValueWrite(value);
   }
 
   public listenFirst(): Promise<T> {
-    if (this.valueProvided) {
-      return new Promise((resolve) => {
-        resolve(this.value);
-      });
-    } else {
-      return new Promise((resolve) => {
-        // I believe, in theory, the value could be set before this callback function is executed
-        if (this.valueProvided) {
-          resolve(this.value);
-        } else {
-          this.requestFirstQueue.push(resolve);
-        }
-      });
-    }
+    return this.delegate.once();
   }
 
   public get(): T {
-    return this.value;
+    return this.delegate.get();
   }
 
   public isSet(): boolean {
-    return this.valueProvided;
+    return this.delegate.isSet();
   }
 
   public set(value: T): void {
-    this.valueProvided = true;
-    this.value = value;
-    for (const callback of this.requestFirstQueue) {
-      callback(value);
-    }
-    for (const callback of this.listeners.values()) {
-      callback(value);
-    }
-    this.requestFirstQueue = [];
+    this.delegate.set(value);
   }
 
   public listen(callback: (value?: T) => void): Stoppable {
-    const id = this.nextListenerId++;
-    this.listeners.set(id, callback);
-    if (this.valueProvided) {
-      callback(this.value);
-    }
-    return {
-      stop: () => {
-        this.listeners.delete(id);
-      }
-    }
+    return this.delegate.listen(callback);
   }
 }
