1cd7af3478eb6738756f12ecd4a77944a171a921
Nils <nils.yseboodt@gmail.com>
2022-11-20 04:43:44 +0100

    simple solution to do tool rolls

--- src/scripts/override/item.ts
+++ src/scripts/override/item.ts
@@ -3,11 +3,18 @@ import { staticValues } from "../static-values";
 import { MyActor, MyItem } from "../types/fixed-types";
 
 // Source: https://gitlab.com/foundrynet/dnd5e/-/blob/e0e7ac62469046b0bf51a2fa933b7be34a476b2e/module/item/entity.js#L415
-async function roll(this: MyItem, {rollMode, createMessage=true}: {configureDialog?: boolean, rollMode?: ClientSettings.Values[`core.rollMode`], createMessage?: boolean} = {}) {
-  return this.displayCard({rollMode, createMessage});;
+async function roll(this: MyItem, wrapped: (...args: any) => any, ...args: [{configureDialog?: boolean, rollMode?: ClientSettings.Values[`core.rollMode`], createMessage?: boolean}]) {
+  const [options] = args;
+  if (this.type === 'tool') {
+    return wrapped(...args);
+  }
+  return this.displayCard({rollMode: options.rollMode, createMessage: options.createMessage});
 }
 
-async function displayCard(this: Item, {rollMode, createMessage=true}: {rollMode?: ClientSettings.Values[`core.rollMode`], createMessage?: boolean} = {}): Promise<ChatMessage> {
+async function displayCard(this: MyItem, wrapped: (...args: any) => any, ...args: [{rollMode?: ClientSettings.Values[`core.rollMode`], createMessage?: boolean}]): Promise<ChatMessage> {
+  if (this.type === 'tool') {
+    return wrapped(...args);
+  }
   let token: TokenDocument = ((this.actor as any).token);
   if (!token) {
     // In 0.8.9, actor.token seems very broken, might be fixed in later versions?
@@ -29,7 +36,7 @@ async function displayCard(this: Item, {rollMode, createMessage=true}: {rollMode
 
 export function registerHooks(): void {
   Hooks.on('setup', () => {
-    libWrapper.register(staticValues.moduleName, 'CONFIG.Item.documentClass.prototype.roll', roll, 'OVERRIDE');
-    libWrapper.register(staticValues.moduleName, 'CONFIG.Item.documentClass.prototype.displayCard', displayCard, 'OVERRIDE');
+    libWrapper.register(staticValues.moduleName, 'CONFIG.Item.documentClass.prototype.roll', roll, 'MIXED');
+    libWrapper.register(staticValues.moduleName, 'CONFIG.Item.documentClass.prototype.displayCard', displayCard, 'MIXED');
   });
 }
