b06cbfff75cb4bcbf2e71b2f5f62104f85f7aa16
Nils <nils.yseboodt@gmail.com>
2023-06-20 23:08:19 +0200

    simpify schema convertor

--- src/scripts/types/document-schema-convertor.d.ts
+++ src/scripts/types/document-schema-convertor.d.ts
@@ -1,40 +1,14 @@
-import type { M } from "ts-algebra";
-
-export type SchemaTypeOf<T> = T extends Array<infer F>
- ? (M.$Array<SchemaTypeOf<F>>)
- : (T extends boolean | number | string
-  ? M.Primitive<T>
-  : (
-    T extends M.Type
-    ? T
-    : (T extends Function | Date | Symbol | undefined
-      ? M.Any
-      :(
-      T extends object
-      ? (
-        M.$Object<
-        {
-          [key in keyof T]: (T[key] extends {isCollection: true}
-            // ? (EmbeddedCollection<T[key]['type'][keyof T[key]['type']], any>)
-            ? (M.Any)
-            // ? (M.Primitive<number>)
-            // ? SchemaOf<{test: true}>
-            : (T[key] extends (DocumentField<infer F>)
-              ? (
-                F extends boolean | number | string
-                  ? M.Primitive<F>
-                  : SchemaTypeOf<F>
-              )
-              : SchemaTypeOf<T[key]>
-            )
-          )
-        },
-        keyof T // TODO everything is optional now
-      >
+type SchemaTypeOf<T> = T extends Array<infer F>
+ ? (Array<SchemaTypeOf<F>>)
+ : (T extends DocumentField<any> & {isCollection: true}
+  ? EmbeddedCollection<any, any>
+  : (T extends (DocumentField<infer F>)
+    ? SchemaTypeOf<F>
+    : (T extends object 
+        ? ({[P in keyof T]: SchemaTypeOf<T[P]>})
+        : T
       )
-      : M.Any
-    ))
+    )
   )
- )
 
-export type SchemaOf<T> = M.$Resolve<SchemaTypeOf<T>>
+export type SchemaOf<T> = $SchemaTypeOf<T>;
