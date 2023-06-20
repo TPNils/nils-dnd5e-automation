c014846754f6353bd53bb575c13a120f53778d71
Nils <nils.yseboodt@gmail.com>
2023-06-21 00:04:52 +0200

    implement optional field types

--- src/scripts/types/document-schema-convertor.d.ts
+++ src/scripts/types/document-schema-convertor.d.ts
@@ -1,14 +1,19 @@
-type SchemaTypeOf<T> = T extends Array<infer F>
- ? (Array<SchemaTypeOf<F>>)
+type RequiredKeys<T> = { [K in keyof T]-?: T[K] extends DocumentField<any> ? (T[K] extends {required: true} ? K : never) : ({} extends Pick<T, K> ? never : K) }[keyof T];
+type OptionalKeys<T> = { [K in keyof T]-?: T[K] extends DocumentField<any> ? (T[K] extends {required?: false} ? K : never) : ({} extends Pick<T, K> ? K : never) }[keyof T];
+
+export type SchemaOf<T> = T extends Array<infer F>
+ ? (Array<SchemaOf<F>>)
  : (T extends DocumentField<any> & {isCollection: true}
   ? EmbeddedCollection<any, any>
   : (T extends (DocumentField<infer F>)
-    ? SchemaTypeOf<F>
+    ? SchemaOf<F>
     : (T extends object 
-        ? ({[P in keyof T]: SchemaTypeOf<T[P]>})
+        ? ({
+          [P in RequiredKeys<T>]-?: SchemaOf<T[P]>;
+        } & {
+          [P in OptionalKeys<T>]?: SchemaOf<T[P]>;
+        })
         : T
       )
     )
   )
-
-export type SchemaOf<T> = $SchemaTypeOf<T>;
